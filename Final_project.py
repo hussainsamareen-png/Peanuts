4afd7f41dc3af5e023374d0d1a1f7af982708d78
briana.castilla <briana.castilla@gmail.com>
2026-01-19 16:56:11 +0100

    Zodiacs comment

--- Final_project.py
+++ Final_project.py
@@ -12,3 +12,18 @@ with open("data\A_people.json", encoding='utf-8') as file:
 # for letter in abc:
 #     with open(f"data\People\{letter}_people.json", encoding='utf-8') as file:
 #     people= json.load(file)
+
+# Assign each person a zodiac sign, adding a new key:value pair
+# zodiac:xxx
+# Capricorn: December 21 - January 19
+# Aquarius: January 20 - February 18
+# Pisces: February 19 - March 20
+# Aries: March 21 - April 19
+# Taurus: April 20 - May 20
+# Gemini: May 21 - June 20
+# Cancer: June 21 - July 22
+# Leo: July 23 - August 22
+# Virgo: August 23 - September 22
+# Libra: September 23 - October 22
+# Scorpio: October 23 - November 21
+# Sagittarius: November 22 - December 20
