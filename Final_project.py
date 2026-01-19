eacd5b8e0d53983c91b3401a4a710ef3c8d2cee0
briana.castilla <briana.castilla@gmail.com>
2026-01-19 16:18:47 +0100

    First code

--- Final_project.py
+++ Final_project.py
@@ -1,1 +1,14 @@
 # data/*.json --> ignore file command.
+
+import json
+with open("data\A_people.json", encoding='utf-8') as file:
+    Apeople= json.load(file)
+
+
+# abc= ['A',"B", "C", 'D', 'E', 'F', 'G', "H",
+#        'I', 'J', 'K', 'L', "M", "N", "O", 'P',
+#          "Q", "R", 'S', 'T', "U", "V", 'W', 'X', "Y", "Z"]
+
+# for letter in abc:
+#     with open(f"data\People\{letter}_people.json", encoding='utf-8') as file:
+#     people= json.load(file)
