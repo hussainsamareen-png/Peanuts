cb170b50252c3584547faf2f8135ddde2a513c6a
sdeliysky <s.deliysky@gmail.com>
2026-01-20 16:29:05 +0200

    Make .csv file with one profession per row

--- Final_project.py
+++ Final_project.py
@@ -204,13 +204,16 @@ for dict in allpeople:
 print(data)
 import csv
 
+# Make .csv file with one profession per row
 with open('profession_by_zodiac.csv', mode='w',  newline='', encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerows(data)
+    for zodiac, professions in data:
+        for profession in professions:
+            row = [zodiac, profession]
+            writer.writerow(row)
 
-
-for list in data:
-    for 
+#print(writer)
 
 # Assign each person a zodiac sign, adding a new key:value pair
 # zodiac:xxx
