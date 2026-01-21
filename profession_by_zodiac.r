2606ff56e67c56affc274faec0573e6083a10de5
sdeliysky <s.deliysky@gmail.com>
2026-01-21 17:07:27 +0200

    Changed counts to percentages

--- profession_by_zodiac.r
+++ profession_by_zodiac.r
@@ -2,25 +2,29 @@
 
 library(tidyverse)
 
+# Note: make this into a loop for all of the files needed, so it can do all at once
+# for .csv file in folder
+
 # Load data
 data <- read_csv("profession_by_zodiac.csv")
 
 # Make table with zodiac and one profession per row
-data_table <- data |>
+data_table_zodiac <- data |>
   select(Zodiac, Profession)
 
-# Count the amount of times each profession appears
-profession_counts <- data |>
-  filter(Zodiac != "Zodiac", Profession != "Profession") |>
-  group_by(Profession) |>
-  summarise(Count = n(), .groups = "drop")
+# Make a list of the tables (each entry in the list is a table) that count:
+counts <- list(
+  by_profession = data_table_zodiac |> count(Profession, name = "Count"), # amount of people in each profession
+  by_zodiac = data_table_zodiac |> count(Zodiac, name = "Count") # amount of people with each zodiac sign
+)
 
 # Find the three most common professions
-professions_rank <- profession_counts |>
+professions_rank <- counts$by_profession |>
   arrange(desc(Count)) |>
   slice_head(n = 3)
 
-count <- 0
+# Add a list in which the tables for the visualisation will be saved
+tables <- list()
 
 # Loop for three most common professions 
 for (i in 1:3){
@@ -28,52 +32,38 @@ for (i in 1:3){
   # Extract profession and make zodiac table for each
   profession_i <- professions_rank$Profession[i]
 
-  # Count how many times that profession appears per zodiac and make a table for each
-  table_i <- data_table |>
+  # Count how many times that profession appears 
+  table_i <- data_table_zodiac |>
     filter(Profession == profession_i) |>
     group_by(Zodiac) |>
-    summarise(Count = n(), .groups = "drop")
-
-  # Save to specific table for each profession
-  assign(paste0("zodiac_profession", i), table_i)
-  assign(paste0("profession", i), profession_i)
-}
-
-# Make a list of all the the tables
-tables <- list(
-  zodiac_profession1,
-  zodiac_profession2,
-  zodiac_profession3
-)
-
-# Loop for plotting the three barplots
-for (i in 1:3) {
-
-  # Track which profession each barplot is for
-  profession_i <- professions_rank$Profession[i]
-  table_i <- tables[[i]]
+    summarise(Count = n(), .groups = "drop") |>
+    mutate(Percentage = Count / counts$by_zodiac$Count[match(Zodiac, counts$by_zodiac$Zodiac)] * 100) |>
+    arrange(desc(Percentage)) |> 
+    mutate(Zodiac = factor(Zodiac, levels = Zodiac))
 
   # Create barplot
   barplot_i <- table_i |>
     ggplot(
       aes(
         x = Zodiac, 
-        y = Count, 
+        y = Percentage, 
         fill = Zodiac)
       ) +
     geom_col(fill = 'darkgrey') +
       labs(
         x = 'Zodiac sign', 
         y = paste0('Number of ', profession_i, 's'), 
-        title = paste0('Distribution of ', profession_i, 's by zodiac sign')
+        title = paste0('Percentage of ', profession_i, 's per zodiac sign %')
     ) +
     # Formatting to make the barplots prettier
-    ylim(0, 500) +  # same y-axis for all barplots
-    geom_hline(yintercept = seq(100, 500, by = 100), color = "grey", linetype = "dashed", linewidth = 0.5)
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    ylim(0, 30) +  # same y-axis for all barplots
+    geom_hline( # formatting and aesthetics
+      yintercept = seq(5, 30, by = 5),
+      color = "grey", 
+      linetype = "dashed"
+    )
 
-   # Save the barplot as .png
+  # Save the barplot as .png
   ggsave(
     filename = paste0("zodiac_barplot", i, ".png"),
     plot = barplot_i
